f49a8fdd3878ed9ddde45490ce7a90f1e409b087
Vlad Stepaniuk <vladstepaniuk44@outlook.com>
2022-01-07 16:20:35 +0200

    Initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using DESystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESystem.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody] LoginModel credentials)
+        {
+            if (credentials == null)
+                return BadRequest("Invalid client request");
+
+            if(credentials.Username == "" && credentials.Password == "")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SuperSecretKey#345"));
+                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "http://localhost:16087",
+                    audience: "http://localhost:16087",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: signingCredentials
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
